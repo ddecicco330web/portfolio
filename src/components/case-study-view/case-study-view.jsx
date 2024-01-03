c4132add72fb274ec703b9155d6924ba5367465d
ddecicco330web <ddecicco330@gmail.com>
2024-01-02 20:06:01 -0500

    Add animations to case study view

--- src/components/case-study-view/case-study-view.jsx
+++ src/components/case-study-view/case-study-view.jsx
@@ -1,67 +1,73 @@
 import { Row, Col, Image } from 'react-bootstrap';
+import './case-study-view.scss';
+import { Link } from 'react-router-dom';
 
 const CaseStudyView = ({ project }) => {
   return (
     <Row>
       <Col>
-        <h1>{project.name}</h1>
-        <div>
+        <Link to="/projects" className="custom-btn link-btn">
+          {'<- Back to Projects'}
+        </Link>
+        <h1 className={'case-study-section appear'}>{project.name}</h1>
+
+        <div className={'case-study-section appear'}>
           <h2>Overview</h2>
           <p>{project.overview}</p>
         </div>
-        <div className="w-100">
+        <div className={'w-100 case-study-img-container slide-left'}>
           <Image
-            className="w-100"
+            className={'w-100 case-study-section appear'}
             src={process.env.PUBLIC_URL + '/' + project.case_study_imgs[0]}
             alt="project image 1"
           />
         </div>
-        <div>
+        <div className={'case-study-section appear'}>
           <h2>Purpose</h2>
           <p>{project.purpose}</p>
         </div>
-        <div>
+        <div className={'case-study-section appear'}>
           <h2>Objective</h2>
           <p>{project.objective}</p>
         </div>
-        <div className="w-100">
+        <div className={'w-100 case-study-img-container slide-left'}>
           <Image
-            className="w-100"
+            className={'w-100 case-study-section appear'}
             src={process.env.PUBLIC_URL + '/' + project.case_study_imgs[1]}
             alt="project image 2"
           />
         </div>
-        <div>
+        <div className={'case-study-section appear'}>
           {project.approach.map((section) => {
             return (
-              <div>
+              <div key={section.title}>
                 <h2>{section.title}</h2>
                 <p>{section.description}</p>
                 <ul>
                   {section.steps.map((step) => {
-                    return <li>{step}</li>;
+                    return <li key={step}>{step}</li>;
                   })}
                 </ul>
               </div>
             );
           })}
         </div>
-        <div className="w-100">
+        <div className={'w-100 case-study-img-container slide-left'}>
           <Image
-            className="w-100"
+            className={'w-100 case-study-section appear'}
             src={process.env.PUBLIC_URL + '/' + project.case_study_imgs[2]}
             alt="project image 3"
           />
         </div>
-        <div>
+        <div className={'case-study-section appear'}>
           <h2>Challenges</h2>
           <p>{project.challenges}</p>
         </div>
-        <div>
+        <div className={'case-study-section appear'}>
           <h2>Improvements</h2>
           <p>{project.improvements}</p>
         </div>
-        <div>
+        <div className={'case-study-section appear'}>
           <h2>Final Thoughts</h2>
           <p>{project.conclusion}</p>
         </div>
